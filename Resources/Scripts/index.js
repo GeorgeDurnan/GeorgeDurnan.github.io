101188ac5b49d652c18c09627577b38a28a3ca56
georgedurnan <gfjdurnan@gmail.com>
2025-10-28 12:26:43 +0000

    Added JS + Responsive design

---
+++ Resources/Scripts/index.js
@@ -0,0 +1,13 @@
+projects = document.querySelectorAll(".project")
+
+const projectImages = ["/Resources/media/projectScreenshots/cheatSheet.png", "/Resources/media/projectScreenshots/styleGuide.png", "/Resources/media/projectScreenshots/nooshe.png"]
+
+projects.forEach((project, index) => {
+    project.addEventListener("mouseenter", () => {
+        document.getElementById("frame").style.display = "block"
+        document.getElementById("frame").src = projectImages[index]
+    })
+});
+document.getElementById("projectList").addEventListener("mouseleave", () =>{
+    document.getElementById("frame").style.display = "none"
+})
